f4eaf7f5ddbb791e2fbd008f0e0ee9b76927a735
Hasinur <hasinur.rahman1332@gmail.com>
2025-04-01 22:12:48 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-center my-20 text-4xl font-bold text-green-400">
+        Green Impact
+      </h1>
+    </div>
+  );
+};
+
+export default App;
