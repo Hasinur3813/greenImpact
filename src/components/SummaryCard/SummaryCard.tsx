cea6580ac4ffc0c408551c1f1a14c877954731c3
Hasinur <hasinur.rahman1332@gmail.com>
2025-04-14 21:33:18 +0600

    added types & split component in admin overview

---
+++ src/components/SummaryCard/SummaryCard.tsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+const SummaryCard = ({
+  icon,
+  title,
+  value,
+}: {
+  icon: React.ReactNode;
+  title: string;
+  value: string | number;
+}) => (
+  <motion.div
+    className="bg-white p-6 rounded-xl shadow-md flex items-center gap-4"
+    whileHover={{ scale: 1.02 }}
+    transition={{ duration: 0.2 }}
+  >
+    <div className="text-3xl text-primaryColor">{icon}</div>
+    <div>
+      <h4 className="text-sm text-gray-500">{title}</h4>
+      <p className="text-xl font-bold">{value}</p>
+    </div>
+  </motion.div>
+);
+
+export default SummaryCard;
--- src/pages/Admin/Overview/Overview.tsx
+++ src/pages/Admin/Overview/Overview.tsx
@@ -13,15 +13,12 @@ import {
   FaUsers,
   FaDonate,
   FaCalendarAlt,
-  FaPlus,
-  FaUserCog,
   FaMoneyBillWave,
 } from "react-icons/fa";
-import { motion } from "framer-motion";
-import { Link } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Loader from "../../../components/Loader/Loader";
+import SummaryCard from "../../../components/SummaryCard/SummaryCard";
 
 const COLORS = ["#00C49F", "#FF8042"];
 
@@ -109,6 +106,31 @@ const Overview = () => {
               <Tooltip />
             </PieChart>
           </ResponsiveContainer>
+
+          {/* Role Distribution Details */}
+          <div className="mt-6 flex justify-center gap-3 space-y-2">
+            {overview?.roleDistribution.map(
+              (
+                role: { name: "volunteer" | "donor"; value: number },
+                index: number
+              ) => (
+                <div key={index} className=" text-sm">
+                  <div className="flex items-center gap-2">
+                    {/* Color Indicator */}
+                    <span
+                      className="w-4 h-4 rounded-full"
+                      style={{ backgroundColor: COLORS[index % COLORS.length] }}
+                    ></span>
+                    {/* Role Name */}
+                    <span>
+                      {role.name}:{" "}
+                      <span className="font-semibold">{role.value}</span>
+                    </span>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
         </div>
       </div>
 
@@ -117,77 +139,73 @@ const Overview = () => {
         <div className="bg-white p-6 rounded-xl shadow-md">
           <h3 className="text-xl font-semibold mb-4">Recent Users</h3>
           <ul className="space-y-3">
-            {overview?.recentUsers.map((user, i) => (
-              <li key={i} className="flex justify-between">
-                <span>{user.name}</span>
-                <span className="text-sm bg-gray-200 px-2 py-1 rounded-full">
-                  {user.role}
-                </span>
-              </li>
-            ))}
+            {overview?.recentUsers.map(
+              (
+                user: {
+                  name: string;
+                  role: "volunteer" | "donor";
+                  createdAt: Date;
+                },
+                i: number
+              ) => (
+                <li key={i} className="flex justify-between">
+                  <span>{user.name}</span>
+                  <span className="text-sm bg-gray-200 px-2 py-1 rounded-full">
+                    {user.role}
+                  </span>
+                </li>
+              )
+            )}
           </ul>
         </div>
 
         <div className="bg-white p-6 rounded-xl shadow-md">
           <h3 className="text-xl font-semibold mb-4">Recent Donations</h3>
           <ul className="space-y-3">
-            {overview?.donations.recentDonations.map((donation, i) => (
-              <li key={i} className="flex justify-between">
-                <span>{donation.donor.name}</span>
-                <span>${donation.amount}</span>
-                <span>${donation.transactionId.slice(0, 15)}...</span>
-              </li>
-            ))}
+            {overview?.donations.recentDonations.map(
+              (
+                donation: {
+                  amount: number;
+                  transactionId: string;
+                  createdAt: Date;
+                  donor: { name: string; email: string };
+                },
+                i: number
+              ) => (
+                <li key={i} className="flex justify-between">
+                  <span>{donation.donor.name}</span>
+                  <span>${donation.amount}</span>
+                  <span>${donation.transactionId.slice(0, 15)}...</span>
+                </li>
+              )
+            )}
           </ul>
         </div>
       </div>
 
-      {/* Quick Actions */}
+      {/* Quick Actions
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <ActionCard icon={<FaPlus />} label="Add Event" />
         <Link to={"/dashboard/manage-users"} className="block w-full">
           <ActionCard icon={<FaUserCog />} label="Manage Users" />
         </Link>
-      </div>
+      </div> */}
     </section>
   );
 };
 
-// Summary card component
-const SummaryCard = ({
-  icon,
-  title,
-  value,
-}: {
-  icon: React.ReactNode;
-  title: string;
-  value: string | number;
-}) => (
-  <motion.div
-    className="bg-white p-6 rounded-xl shadow-md flex items-center gap-4"
-    whileHover={{ scale: 1.02 }}
-    transition={{ duration: 0.2 }}
-  >
-    <div className="text-3xl text-primaryColor">{icon}</div>
-    <div>
-      <h4 className="text-sm text-gray-500">{title}</h4>
-      <p className="text-xl font-bold">{value}</p>
-    </div>
-  </motion.div>
-);
-
 // Action button/card
-const ActionCard = ({
-  icon,
-  label,
-}: {
-  icon: React.ReactNode;
-  label: string;
-}) => (
-  <button className="flex items-center justify-center space-x-3 p-4 bg-primaryColor text-white font-semibold rounded-xl w-full cursor-pointer hover:bg-secondaryColor transition">
-    <span className="text-xl">{icon}</span>
-    <span>{label}</span>
-  </button>
-);
+// const ActionCard = ({
+//   icon,
+//   label,
+// }: {
+//   icon: React.ReactNode;
+//   label: string;
+// }) => (
+//   <button className="flex items-center justify-center space-x-3 p-4 bg-primaryColor text-white font-semibold rounded-xl w-full cursor-pointer hover:bg-secondaryColor transition">
+//     <span className="text-xl">{icon}</span>
+//     <span>{label}</span>
+//   </button>
+// );
 
 export default Overview;
